5be333e2f5ba338868931b6c5ee746d3c461707a
Shikhin <2030677+shikhin21@users.noreply.github.com>
2025-03-14 11:26:43 -0400

    try adding dist

---
+++ dist/commonTypes.d.ts
@@ -0,0 +1,7 @@
+export type ID = string | number;
+export interface ApiResponse<T> {
+    data: T;
+    success: boolean;
+    message?: string;
+}
+//# sourceMappingURL=commonTypes.d.ts.map
---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+export * from "./commonTypes";
+//# sourceMappingURL=index.d.ts.map
