f0f4c6726c65c4caf0177918a5469ed5cc0c2045
Shikhin <2030677+shikhin21@users.noreply.github.com>
2026-01-20 14:51:23 -0500

    update user type

--- src/types/metadata.ts
+++ src/types/metadata.ts
@@ -2,6 +2,6 @@ export type MetadataType <SupportedLanguageCodeType extends string | number | sy
   id: string,
   slug: string,
   updatedAt?: FieldValueLike,
-  lastUpdatedBy?: 'ADMIN' | 'BATCH',
+  lastUpdatedBy?: 'ADMIN' | 'BATCH_TRANSLATION_SYNC' | 'BATCH_TOPICS_GENERATE',
   en: { singular: string, plural?: string}
 } & Omit<Partial<{ [key in SupportedLanguageCodeType] : {singular: string, plural?: string} }>, 'en'>;
