bb896a60da6eddedfb9f8569198944eb680c4b76
Justin Emery <justinemery@gmail.com>
2026-04-28 21:18:09 +0100

    Additions to support new subtitle solution

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@ export * from './types/dynamicPanels.js';
 export * from './types/firebase.js';
 export * from './types/payments.js';
 export * from './types/metadata.js';
+export * from './types/subtitles.js';
 
